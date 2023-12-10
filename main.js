8fbd3b9de5df87ff8c4f7c930e144ea35b4c7ca2
Affun <affangv8@gmail.com>
2023-12-10 17:21:00 +0530

    im done

--- main.js
+++ main.js
@@ -2,20 +2,36 @@ const app = document.getElementById("app");
 const input = document.getElementById("hex-input");
 const answer = document.getElementById("answer");
 const submitBtn = document.getElementById("submit");
+const half = document.getElementById("half");
 
 let theHex = "#000000";
 let answerState = false;
 let difficulty = 0;
+
+const difficultyBtns = [];
+
+for (let i = 1; i <= 4; i++) {
+  const element = document.querySelector(`#d${i}`);
+  difficultyBtns.push(element);
+}
 
 const randomHex = () => {
   // let hex = "0123456789ABCDEF";
   // let hex = "13579BDF";
   let hex = ["37BF", "13579BDF", "0123456789ABCDEF"];
   let color = "#";
-  for (let i = 0; i < 3; i++) {
-    const pColor = hex[difficulty][Math.floor(Math.random() * hex[difficulty].length)];
-    color += pColor.repeat(2);
+  if (difficulty < 3) {
+    for (let i = 0; i < 3; i++) {
+      let pColor = hex[difficulty][Math.floor(Math.random() * hex[difficulty].length)];
+      color += pColor.repeat(2);
+    }
+    return color;
   }
+  for (let i = 0; i < 6; i++) {
+    let pColor = hex[2][Math.floor(Math.random() * hex[2].length)];
+    color += pColor;
+  }
+
   return color;
 };
 
@@ -29,9 +45,21 @@ const changeHex = () => {
   answer.innerHTML = "answer: " + theHex;
 };
 
+const changeDif = (dif) => {
+  answerState = false;
+  changeHex();
+  difficulty = dif;
+  difficultyBtns.forEach((btn) => {
+    btn.classList.remove("s-dif");
+  });
+  difficultyBtns[dif].classList.add("s-dif");
+};
+
 function submit() {
   if (answerState) {
     answerState = false;
+    input.value = "";
+    half.style.backgroundColor = "";
     changeHex();
     return;
   }
@@ -61,10 +89,18 @@ function submit() {
   submitBtn.innerHTML = "Next";
 
   if (finalHex === theHex) {
-    app.style.backgroundColor = "#22ee22";
+    app.style.backgroundColor = "#0CC078";
     return;
   }
-  app.style.backgroundColor = "#ee2222";
+  app.style.backgroundColor = "#E03C32";
+  setTimeout(() => {
+    if (!answerState) {
+      return;
+    }
+    app.style.backgroundColor = theHex;
+    half.style.backgroundColor = finalHex;
+    return;
+  }, 1500);
 }
 
 input.addEventListener("keyup", (e) => {
@@ -73,3 +109,4 @@ input.addEventListener("keyup", (e) => {
   }
 });
 changeHex();
+difficultyBtns[difficulty].classList.add("s-dif");
